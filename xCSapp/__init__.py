5c6e66a245af93a28de8147303b0a70599871c4f
Kissinger333 <mjy010711@gmail.com>
2025-01-07 07:20:47 +0800

    modifyprofile

--- xCSapp/__init__.py
+++ xCSapp/__init__.py
@@ -8,6 +8,7 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
